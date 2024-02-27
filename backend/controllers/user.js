4949d80aa6e94eedeb589fa21d6cb67774758da7
trickster1o1 <nischaltuladhar15@gmail.com>
2024-02-27 15:30:48 +0545

    user login / signup init

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,11 +1,37 @@
-const user = require('../models/user');
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 const loginUser = async (req, res) => {
-    res.json({msg: 'logged in'});
-}
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    // create JWT
+    const token = createToken(user._id);
+    return res.status(200).json({email, token});
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+  res.json({ msg: "logged in" });
+};
 
 const signupUser = async (req, res) => {
-    res.json({msg: 'signed up'});
-}
+  const { email, password } = req.body;
 
-module.exports = {loginUser, signupUser}
+  try {
+    const user = await User.signup({ email, password });
+
+    // JWT creation
+    const token = createToken(user._id);
+    return res.status(200).json({ email, token });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+  res.json({ msg: "signed up" });
+};
+
+module.exports = { loginUser, signupUser };
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
-const userShcema = new Schema({
+const userSchema = new Schema({
     email: {
         type: String,
         required: true,
@@ -12,4 +14,44 @@ const userShcema = new Schema({
     }
 });
 
-module.exports = mongoose.model('User', userShcema);
+// signup function
+userSchema.statics.signup = async function({email, password}){
+    if(!email || !password) {
+        throw Error('All filds are required.');
+    }
+
+    if(!validator.isEmail(email)) {
+        throw Error('Email is not valid!');
+    } 
+
+    const exist = await this.findOne({email});
+    if(exist) {
+        throw Error('Email already exists.');
+    }
+    
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({email, password: hash});
+
+    return user;
+}
+
+userSchema.statics.login = async function(email, password) {
+    if(!email || !password) {
+        throw Error('All filds are required');
+    }
+
+    const user = await this.findOne({email});
+    if(!user) {
+        throw Error('Incorrect Cardantials!');
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if(!match) {
+        throw Error('Incorrect Cardantials!!!');
+    }
+    return user;
+}
+
+module.exports = mongoose.model('User', userSchema);
